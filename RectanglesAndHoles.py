b38a1b44140b3a0ffb1e78bc249b4a30f44847df
Chris Wilkes <cwilkes@gmail.com>
2014-05-23 22:02:14 -0700

    brass knuckles on left

--- RectanglesAndHoles.py
+++ RectanglesAndHoles.py
@@ -1,3 +1,300 @@
+from operator import attrgetter
+import sys
+import datetime
+
+def lgi(format, *msg):
+    print >>sys.stderr, format % msg
+    sys.stderr.flush()
+
+
+class CoverBoard(object):
+    def __init__(self, board):
+        self.board = board
+        self.widths, self.heights, self.indexes = list(), list(), list()
+        for t in board.tiles:
+            self.widths.append((t.col, t.col+t.width-1))
+            self.heights.append((t.row, t.row+t.height-1))
+            self.indexes.append(t.index)
+
+    def find_min_col(self, col, row):
+        ret = -sys.maxint
+        for pos, (min_row, max_row) in enumerate(self.heights):
+            if min_row <= row <= max_row:
+                min_col, max_col = self.widths[pos]
+                if col >= max_col:
+                    ret = max(ret, max_col)
+        return ret
+
+    def __contains__(self, item):
+        for pos, (min_col, max_col) in enumerate(self.widths):
+            if min_col <= item[0] <= max_col:
+                min_row, max_row = self.heights[pos]
+                if min_row <= item[1] <= max_row:
+                    lgi('hit on # %d with tile %s for col %d <= %d <= %d and row %d <= %d <= %d',
+                        pos, self.board[self.indexes[pos]], min_col, item[0], max_col, min_row, item[1], max_row)
+                    return True
+        return False
+
+
+class Board(object):
+    def __init__(self, A, B):
+        self.tiles = list()
+        for pos, (w, h) in enumerate(zip(A, B)):
+            self.tiles.append(Tile(len(self.tiles), w, h))
+
+    def __getitem__(self, item):
+        return self.tiles[item]
+
+    def __iter__(self):
+        return self.tiles.__iter__()
+
+    def __len__(self):
+        return len(self.tiles)
+
+    def get_by_max_dim(self):
+        return sorted(self.tiles, key=attrgetter('max_dim'))
+
+    def __nonzero__(self):
+        return len(self.tiles) != 0
+
+    def make_cover_board(self):
+        return CoverBoard(self)
+
+    def array_out(self):
+        ret = list()
+        for tile in self.tiles:
+            ret.append(tile.col)
+            ret.append(tile.row)
+            ret.append(tile.direction)
+        return ret
+
+
+class Tile(object):
+    def __init__(self, index, width, height, col=0, row=0):
+        self.index = index
+        self.width, self.height = width, height
+        self.col, self.row = col, row
+        self.direction = 0
+        self.max_dim = max(self.width, self.height)
+        self.min_dim = min(self.width, self.height)
+
+    def flip(self):
+        self.direction = (self.direction+1) % 2
+        self.width, self.height = self.height, self.width
+        return self
+
+    def move(self, col, row):
+        self.col, self.row = col, row
+        return self
+
+    def bump(self, col=0, row=0):
+        return self.move(self.col + col, self.row+row)
+
+    def rotate_longway(self, direction):
+        if self.height > self.width:
+            if direction == 0:
+                self.flip()
+        else:
+            if direction == 1:
+                self.flip()
+        return self
+
+    def __str__(self):
+        return 'Rectangle %d (w:%d,h:%d) is (%d, %d) - (%d, %d)' % (self.index, self.width, self.height, self.col, self.row, self.col+self.width, self.row+self.height)
+
+
+def _outside_box(board):
+    total_length = 0
+    for tile in board:
+        total_length += tile.max_dim
+    tile_ids = [_.index for _ in sorted(board, key=attrgetter('max_dim'), reverse=True)]
+    lgi('total length %d', total_length)
+    side_length = (total_length * 0.95) / 5
+    # can have some overlap in the starting corner
+    tile = board[tile_ids.pop(0)]
+    tile.move(0, 0)
+    tile.rotate_longway(0)
+    col = tile.width
+    row = 0
+    while col < side_length and tile_ids:
+        tile = board[tile_ids.pop(0)]
+        tile.rotate_longway(0)
+        tile.move(col, row)
+        col += tile.width
+    while row > -side_length and tile_ids:
+        tile = board[tile_ids.pop(0)]
+        tile.rotate_longway(1)
+        row -= tile.height
+        tile.move(col, row)
+    row += tile.height-1
+    while col > 0 and tile_ids:
+        tile = board[tile_ids.pop(0)]
+        tile.rotate_longway(0)
+        col -= tile.width
+        tile.move(col, row-tile.height)
+    col = 0
+    row_bottom = row
+    max_width = 0
+    while row < 1 and tile_ids:
+        tile = board[tile_ids.pop(0)]
+        tile.rotate_longway(1)
+        tile.move(col-tile.width, row)
+        row += tile.height
+        max_width = max(max_width, tile.width)
+    prev_tile = None
+    col += max_width + 100
+    row = row_bottom
+    inner_wall_ids = list()
+    while row < 0 and tile_ids:
+        tile = board[tile_ids.pop(0)]
+        inner_wall_ids.append(tile.index)
+        tile.rotate_longway(1)
+        if tile.height + row >= 0:
+            tile.move(prev_tile.col + prev_tile.width, 0 - tile.height)
+            break
+        tile.move(col, row)
+        row += tile.height
+        prev_tile = tile
+    return tile_ids, col + max_width + 100, row_bottom, inner_wall_ids
+
+
+def _move_left(board, tile_ids, is_under):
+    furthest_left = list()
+    cb = board.make_cover_board()
+    lgi('cover board done')
+    for tile in (board[_] for _ in tile_ids):
+        min_col = -sys.maxint
+        for row in range(tile.row, tile.row+tile.height):
+            min_col = max(min_col, cb.find_min_col(tile.col-1, row))
+        #lgi('for %s furthest left is %d', tile, min_col)
+        furthest_left.append(min_col)
+    lgi('furthest left %s', furthest_left)
+    sliders = list()
+    for left_col, tile_id in zip(furthest_left, tile_ids):
+        tile = board[tile_id]
+        if left_col == -1:
+            sliders.append(tile)
+        else:
+            tile.move(left_col+1, tile.row)
+    lgi('left with %d sliders', len(sliders))
+    cb = board.make_cover_board()
+    for tile in sliders:
+        if not is_under():
+            break
+        # see what is one row beneath me
+        min_col1 = cb.find_min_col(tile.col-1, tile.row - 1)
+        min_col2 = cb.find_min_col(tile.col-1, tile.row + tile.height)
+        if min_col1 == -1:
+            min_col = min_col2
+        elif min_col2 == -1:
+            min_col = min_col1
+        else:
+            min_col = min(min_col1, min_col2)
+        if min_col != -1:
+            tile.move(min_col+1, tile.row)
+            #lgi('slid1 %s', tile)
+    cb = board.make_cover_board()
+    for tile in sliders:
+        if not is_under():
+            break
+        # see what is one row beneath me
+        min_col1 = cb.find_min_col(tile.col-1, tile.row - 1)
+        min_col2 = cb.find_min_col(tile.col-1, tile.row + tile.height)
+        if min_col1 == -1:
+            min_col = min_col2
+        elif min_col2 == -1:
+            min_col = min_col1
+        else:
+            min_col = min(min_col1, min_col2)
+        if min_col != -1:
+            tile.move(min_col+1, tile.row)
+            #lgi('slid2 %s', tile)
+
+
+def box_layout(board, is_under):
+    tile_ids, col, row_bottom, inner_wall_ids= _outside_box(board)
+    tile_ids = list(reversed(tile_ids))
+    lgi('final (%d): %s', len(tile_ids), tile_ids)
+    total_height = 0
+    for tile in (board[_] for _ in tile_ids):
+        total_height += tile.min_dim
+    spacing = (abs(row_bottom)-total_height) / (len(tile_ids))
+    row = row_bottom+1
+    while tile_ids:
+        tile = board[tile_ids.pop(0)]
+        tile.rotate_longway(0)
+        tile.move(0, row)
+        row += tile.height + spacing
+    lgi('left: %s %s', tile_ids, inner_wall_ids)
+    _move_left(board, inner_wall_ids, is_under)
+
+
+def simple_layout(board):
+    col, row = 0, 0
+    by_col = list()
+    entries = list()
+    max_width = 0
+    for tile in board.get_by_max_dim():
+        tile.move(col, row)
+        if tile.height < tile.width:
+            pass
+        else:
+            tile.flip()
+        row += tile.height
+        max_width = max(max_width, tile.width)
+        entries.append(tile.index)
+        if row > 12000:
+            row = 0
+            col += max_width
+            by_col.append(entries)
+            lgi('by col: %s', entries)
+            entries = list()
+    if entries:
+        by_col.append(entries)
+    return by_col
+
+
+def max_width_in_column(board, by_col):
+    for col_number, entries in enumerate(by_col):
+        max_width = 0
+        min_max_col_row = [sys.maxint, -sys.maxint, sys.maxint, -sys.maxint]
+        for tile in (board[_] for _ in entries):
+            max_width = max(max_width, tile.width)
+            min_max_col_row[0] = min(min_max_col_row[0], tile.col)
+            min_max_col_row[1] = max(min_max_col_row[1], tile.col+tile.width)
+            min_max_col_row[2] = min(min_max_col_row[2], tile.row)
+            min_max_col_row[3] = max(min_max_col_row[3], tile.row+tile.height)
+        lgi('Column %d goes from (%d, %d) to (%d, %d) max width: %d',
+            col_number, min_max_col_row[0], min_max_col_row[2], min_max_col_row[1], min_max_col_row[3], max_width)
+        for tile in (board[_] for _ in entries):
+            if tile.width < tile.height <= max_width:
+                # can flip
+                tile.flip()
+                lgi('flipped', tile)
+        row = board[entries[0]].row + board[entries[0]].height
+        for tile in (board[_] for _ in entries[1:]):
+            delta_row = tile.row - row
+            if delta_row > 0:
+                #tile.move(tile.col, tile.row-delta_row)
+                lgi('squashed', tile)
+            row = tile.row + tile.height
+        # now put the top on on the bottom
+        t = board.tiles[entries[-1]]
+        t.move(min_max_col_row[0], min_max_col_row[2] - t.height)
+        bottom_tile, top_tile =  board.tiles[entries[-1]], board.tiles[entries[-2]]
+        if top_tile.width < bottom_tile.width:
+            top_tile.bump(bottom_tile.width-top_tile.width, 0)
+        i = 1
+        while i < len(entries):
+            t0 = board[entries[i-1]]
+            t1 = board[entries[i]]
+            if t0.width == t1.width:
+                #lgi('flip blocks: %s %s', t0, t1)
+                i+=2
+                pass
+            else:
+                #lgi('different widths: %s %s', t0, t1)
+                i+=1
 
 
 class RectanglesAndHoles(object):
@@ -5,24 +302,15 @@ class RectanglesAndHoles(object):
         pass
 
     def place(self, A, B):
-        col = 0
-        row = 0
-        ret = list()
-        max_height = 0
-        for w, h in zip(A, B):
-            if h < w < max_height:
-                ret.append((col, row, 0))
-                row += h
-                max_height = max(max_height, w)
-            else:
-                ret.append((col, row, 1))
-                row += w
-                max_height = max(max_height, h)
-            if row > 15000:
-                row = 0
-                col += max_height
-        ret2 = list()
-        for r in ret:
-            for _ in r:
-                ret2.append(_)
-        return ret2
+        start_time = datetime.datetime.now()
+        end = start_time + datetime.timedelta(seconds=60)
+        def is_under():
+            return datetime.datetime.now() <= end
+
+        board = Board(A, B)
+        box_layout(board, is_under)
+        delta_time = datetime.datetime.now() - start_time
+        delta_seconds = delta_time.seconds + delta_time.microseconds / 1000000.
+        lgi('Total time %s', delta_seconds)
+        sys.stderr.flush()
+        return board.array_out()
